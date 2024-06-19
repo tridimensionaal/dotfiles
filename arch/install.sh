c4a294f86917fbd0862dedb412ebc490c8aa397e
stack_and_heap <matiaseda@hotmail.cl>
2024-06-19 01:37:18 -0400

    Added arch config

---
+++ arch/install.sh
@@ -0,0 +1,44 @@
+# directories
+OTHERS_DIR="$HOME/Documents/others"
+BASH_SCRIPTS_DIR="$OTHERS_DIR/Bash-scripts-for-daily-task"
+BIN_DIR="$HOME/bin"
+FONTS_DIR="$HOME/.fonts"
+CONFIG_DIR="$HOME/.config"
+DOTFILES_DIR="$OTHERS_DIR/dotfiles"
+
+mkdir -p "$OTHERS_DIR" "$FONTS_DIR" "$BIN_DIR"  "$CONFIG_DIR" "$CONFIG_DIR/tmux"
+
+sudo pacman -S git curl xclip less alacritty firefox zsh tmux discord neovim go
+
+# clone custom repos
+git clone "git@github.com:tridimensionaal/dotfiles.git" "$OTHERS_DIR/dotfiles"
+git clone git@github.com:tridimensionaal/Bash-scripts-for-daily-task.git  "$BASH_SCRIPTS_DIR"
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+##################################### RESTART PC #####################################
+rm "$HOME/.zshrc"
+ln -s "$OTHERS_DIR/dotfiles/oh-my-zsh/.zshrc" "$HOME"
+
+# install custom bash scripts
+ln -s "$BASH_SCRIPTS_DIR/scripts/*" "$BIN_DIR"
+
+# instal gnome conf
+dconf load / < "$DOTFILES_DIR/arch/user.conf"
+
+# install custom fonts 
+wget -O "$FONTS_DIR/fonts.zip" "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Hack.zip"
+unzip "$FONTS_DIR/fonts.zip" -d "$FONTS_DIR"
+rm "$FONTS_DIR/fonts.zip"
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ln -s "$OTHERS_DIR/dotfiles/tmux/tmux.conf" "$CONFIG_DIR/tmux"
+# Enter to $CONFIG_DIR/tmux/tmux.conf y apretar [prefix] + [I]
+# tmux source "$CONFIG_DIR/tmux/tmux.conf"
+
+# install astro vim
+sudo rm -r "$CONFIG_DIR/nvim"
+git clone --depth 1 https://github.com/AstroNvim/template "$CONFIG_DIR/nvim"
+rm -rf ~/.config/nvim/.git
+
--- oh-my-zsh/.zshrc
+++ oh-my-zsh/.zshrc
@@ -1,7 +1,7 @@
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 # export PATH=$PATH:/usr/local/go/bin
-export PATH=$PATH:$HOME/bin/scripts
+export PATH=$PATH:$HOME/bin
 
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
