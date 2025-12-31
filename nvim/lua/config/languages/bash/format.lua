6a496f81b109ad75e5a716c5ffce974adf572397
tridimensionaal <matiaseda@hotmail.cl>
2025-12-31 16:29:55 -0300

    fix/neovim: shfmt tab -> spaces

--- nvim/lua/config/languages/bash/format.lua
+++ nvim/lua/config/languages/bash/format.lua
@@ -1,7 +1,9 @@
 return {
   sources = function(null_ls)
     return {
-      null_ls.builtins.formatting.shfmt,
+      null_ls.builtins.formatting.shfmt.with({
+        extra_args = { "-i", "4" },
+      }),
     }
   end,
   tools = { "shfmt" },
